abdbdf8e3f42d660eb381f2fc846adc6b63a7f5d
Shivang <rshivang12345@gmail.com>
2024-12-12 12:40:46 +0530

    fix: append authorization headers on proxy api

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,6 +1,31 @@
-import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+import {
+  getSession,
+  withMiddlewareAuthRequired,
+} from "@auth0/nextjs-auth0/edge";
 
-export default withMiddlewareAuthRequired();
+import { NextResponse } from "next/server";
+
+export default withMiddlewareAuthRequired(async function middleware(req) {
+  if (!req.nextUrl.pathname.startsWith("/api/auth")) {
+    return NextResponse.next();
+  }
+
+  const requestHeaders = new Headers(req.headers);
+  const user = await getSession();
+  requestHeaders.set("Authorization", `Bearer ${user?.idToken}`);
+  const session = await getSession();
+  const headers = new Headers(req.headers);
+
+  if (session) {
+    headers.append("Authorization", `Bearer ${session.accessToken}`);
+  }
+
+  return NextResponse.next({
+    request: {
+      headers,
+    },
+  });
+});
 
 export const config = {
   matcher: ["/private/:path*", "/api/:path*"],
