00a385909fdd5cdba05753d6d1e7997c44f7d93a
Shivang <rshivang12345@gmail.com>
2024-12-13 10:13:27 +0530

    chore: custom afterCallback in auth0

--- web/app/api/auth/[auth0]/route.ts
+++ web/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,31 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import {
+  AppRouteHandlerFnContext,
+  handleAuth,
+  handleCallback,
+  Session,
+} from "@auth0/nextjs-auth0";
+import { NextRequest } from "next/server";
 
-export const GET = handleAuth();
+const afterCallback = async (
+  req: NextRequest,
+  session: Session,
+  state?: { [key: string]: any }
+) => {
+  console.log("I am getting called after callback", session);
+  if (session.user) {
+    console.log("user found", session.user);
+  } else {
+    console.log("user not found", session);
+  }
+
+  return session;
+};
+
+export const GET = handleAuth({
+  async callback(req: NextRequest, ctx: AppRouteHandlerFnContext) {
+    console.log(ctx);
+    return await handleCallback(req, ctx, {
+      afterCallback,
+    });
+  },
+});
